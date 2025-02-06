1aef8454ab8b95046bca5aa860e804863b9d6e8b
matthz.max <matheus7serafim@gmail.com>
2025-02-06 08:48:12 -0300

    lição feita definitiva

--- src/main/java/com/senai/ecommerce/dto/PedidoDTO.java
+++ src/main/java/com/senai/ecommerce/dto/PedidoDTO.java
@@ -2,9 +2,11 @@ package com.senai.ecommerce.dto;
 
 import java.time.Instant;
 
+import com.senai.ecommerce.enums.StatusDoPedido;
+
 import ch.qos.logback.core.status.Status;
 
-public record PedidoDTO(Instant momento, Status StatusDoPedido) {
+public record PedidoDTO(Instant momento, StatusDoPedido status) {
 
 	
 	
